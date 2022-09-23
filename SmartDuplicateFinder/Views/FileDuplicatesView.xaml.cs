6929e19701dc1a87b424999c706a89ceb5cc9762
NEBA_PC\Administrator <nebyouabate@gmail.com>
2022-09-22 22:32:51 -0500

    Working

--- SmartDuplicateFinder/Views/FileDuplicatesView.xaml.cs
+++ SmartDuplicateFinder/Views/FileDuplicatesView.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace SmartDuplicateFinder.Views
 {
@@ -23,6 +10,14 @@ namespace SmartDuplicateFinder.Views
         public FileDuplicatesView()
         {
             InitializeComponent();
+            AddCommandBindings();
+
+            DataContext = this;
+        }
+
+        private void AddCommandBindings()
+        {
+            // CommandBindings.Add(new CommandBinding(AppCommands.Exit, (sender, args) => Close()));
         }
     }
 }
