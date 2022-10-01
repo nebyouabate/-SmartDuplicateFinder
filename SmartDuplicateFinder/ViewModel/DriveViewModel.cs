ac225d1be492b3adbd862b21a7ee0344c04e5375
Nebyou Abate <nebyouabate@gmail.com>
2022-10-01 15:42:29 -0500

    Adding icons

--- SmartDuplicateFinder/ViewModel/DriveViewModel.cs
+++ SmartDuplicateFinder/ViewModel/DriveViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using PropertyChanged;
+using SmartDuplicateFinder.Utils;
+using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
@@ -10,18 +12,46 @@ namespace SmartDuplicateFinder.ViewModel;
 public class DriveViewModel : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
-    public DriveViewModel(DriveInfo driveInfo)
+
+    static DriveViewModel()
     {
-        DriveInfo = driveInfo;       
-        Name = driveInfo.Name[..^1];
-        DisplayName = $"{driveInfo.VolumeLabel} ({Name})";
+        s_systemDrive = Path.GetPathRoot(Environment.SystemDirectory)!;
     }
 
+    public DriveViewModel(DriveInfo driveInfo)
+    {
+        _driveInfo = driveInfo;
+
+        Icon = _driveInfo.DriveType switch
+        {
+            DriveType.Fixed => _driveInfo.Name.Equals(s_systemDrive, StringComparison.InvariantCultureIgnoreCase) ? Icons.WindowsDrive : Icons.HardDrive,
+            DriveType.Network => _driveInfo.IsReady ? Icons.NetworkConnectedDrive : Icons.NetworkDisconnectedDrive,
+            DriveType.CDRom => Icons.CDRomDrive,
+            DriveType.Removable => Icons.RemovableDrive,
+            _ => Icons.UnknownDrive
+        };
+
+        if (_driveInfo.IsReady)
+        {
+            IsSelectable = true;
+
+            DisplayName = $"{_driveInfo.VolumeLabel} ({_driveInfo.Name[..^1]})";
+            Name = _driveInfo.Name[..^1];
+        }
+        else
+        {
+            IsSelectable = false;
+
+            DisplayName = Name = $"({_driveInfo.Name[..^1]})";
+        }
+    }
+    public Icons Icon { get; private set; }
     public string Name { get; set; }
-
     public string DisplayName { get; set; }
+    public bool IsSelectable { get; set; }
+    public bool IsSelected { get; set; }
 
-    public bool IsSelected { get; set; }    
+    private static readonly string s_systemDrive;
 
-    public DriveInfo DriveInfo { get; private set; }   
+    private readonly DriveInfo _driveInfo;
 }
