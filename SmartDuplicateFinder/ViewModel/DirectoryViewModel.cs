48da23950f6ddd973fd4c11cb74d82496be1e93f
Nebyou Abate <nebyouabate@gmail.com>
2022-10-02 18:06:52 -0500

    Tree view in progress

---
+++ SmartDuplicateFinder/ViewModel/DirectoryViewModel.cs
@@ -0,0 +1,46 @@
+﻿using SmartDuplicateFinder.Utils;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+
+namespace SmartDuplicateFinder.ViewModel;
+
+public class DirectoryViewModel : INotifyPropertyChanged
+{
+
+    public static readonly DirectoryViewModel UnExpanded = new();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    internal DirectoryViewModel(DirectoryInfo directoryInfo) 
+        : this()
+    {
+        DirectoryInfo = directoryInfo;
+
+        DisplayName = DirectoryInfo.Name;
+        Name = DirectoryInfo.Name;
+    }
+
+    private DirectoryViewModel()
+    {
+        DirectoryInfo = null!;
+        
+        DisplayName = "Loading ...";
+        Name = "";
+
+        IsSelected = false;
+        IsExpanded = false;
+
+        SubFolders = new ObservableCollection<DirectoryViewModel> { UnExpanded };
+    }
+
+    public Icons Icon { get; private set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+    //public bool IsSelectable { get; set; }
+    public bool IsSelected { get; set; }
+    public bool IsExpanded { get; set; }
+    public DirectoryInfo DirectoryInfo { get; protected set; }
+    public ObservableCollection<DirectoryViewModel> SubFolders { get; protected set; }
+}
+
